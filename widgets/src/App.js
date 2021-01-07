eb8c8ec5447122824ebd057f14f9aa0a9bcd30fb
xufengFlynn <zhangxufeng26@gmail.com>
2021-01-07 14:31:37 -0800

    expanding the accordion

---
+++ widgets/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework.",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a good JS library among engineers.",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components.",
+  },
+];
+
+export default () => {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+};
