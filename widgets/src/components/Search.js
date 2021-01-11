db4fbbae132badc0ee137e2199a0f6a79639c7b2
xufengFlynn <zhangxufeng26@gmail.com>
2021-01-11 14:19:39 -0800

    Executing the request from useEffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -7,7 +7,15 @@ const Search = () => {
   useEffect(() => {
     // run code here every time 'term' changes
     const search = async () => {
-      await axios.get("asdasd");
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
     };
 
     search();
