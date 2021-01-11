41d54c4c9dd8266bf3b141fcda0468fcf8c0130e
xufengFlynn <zhangxufeng26@gmail.com>
2021-01-11 14:12:38 -0800

    use async in useEffect function

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,20 +1,16 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
 
-  console.log("I run with every render");
-
-  // useEffect(() => {
-  //   console.log("I only run once");
-  // }, []);
-
-  // useEffect(() => {
-  //   console.log("I run after every render and at initial render");
-  // });
-
   useEffect(() => {
-    console.log("I only run once");
+    // run code here every time 'term' changes
+    const search = async () => {
+      await axios.get("asdasd");
+    };
+
+    search();
   }, [term]);
 
   return (
